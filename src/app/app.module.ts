1e99ad7bddfaa89f4bb9503dd2e130a7e9460d8e
3RebeccaSila3 <silarebecca26@gmail.com>
2018-09-10 12:57:10 +0300

    add a component to handle logic on goals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Component } from './.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component
   ],
   imports: [
     BrowserModule
