8e44c90ab9f40b2dc5f9d1c153cde9bc02d202ab
3RebeccaSila3 <silarebecca26@gmail.com>
2018-09-10 15:12:29 +0300

    add event emmitters

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -16,6 +16,12 @@ export class GoalComponent implements OnInit {
     new Goal(6, 'Plot my world domination plan','Cause I am an evil overlord'),
 ]
 
+completeGoal(isComplete,index){
+  if (isComplete){
+    this.goals.splice(index,1)
+  }
+}
+
 toogleDetails(index){
   this.goals[index].showDescription = !this.goals[index].showDescription;
 }
